777d83122df15d253f446b57f978ab525ca1c5a9
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-06-04 15:28:13 -0600

    GO: Add proof-of-concept for cmsdk RGB control server.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jamievlin.github.io/cmkeyboard-http-server
 
 go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -16,8 +16,119 @@
 
 package main
 
-import "fmt"
+// #cgo CFLAGS: -I${SRCDIR}/contrib/cmsdk/include/
+// #cgo LDFLAGS: -L${SRCDIR}/contrib/cmsdk/lib/ -lSDKDLL64
+// #include "SDKDLL.h"
+import "C"
+import (
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var (
+	errorLogger *log.Logger
+)
+
+func init() {
+	errorLogger = log.New(os.Stderr, "[ERROR] ", log.LstdFlags)
+}
+
+func rootHandler(
+	writer http.ResponseWriter,
+	_ *http.Request,
+	_ httprouter.Params) {
+	writer.WriteHeader(http.StatusOK)
+	_, err := writer.Write([]byte("{}"))
+	if err != nil {
+		errorLogger.Print("rootHandler: ", err)
+	}
+}
+
+type putDeviceLedControlBody struct {
+	Enabled bool `json:"enabled"`
+}
+
+func putDeviceLedControl(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	var dev = params.ByName("device")
+	devInt, err := strconv.Atoi(dev)
+	if err != nil {
+		errorLogger.Printf("Device %s unknown!", dev)
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := w.Write([]byte("{}"))
+		if err != nil {
+			errorLogger.Fatal("Cannot write response!", err)
+		}
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err := w.Write([]byte("{}"))
+		if err != nil {
+			errorLogger.Fatal("Cannot write response!", err)
+		}
+		return
+	}
+
+	var bodyParsed putDeviceLedControlBody
+	if json.Unmarshal(body, &bodyParsed) != nil {
+		log.Fatal("error!")
+	}
+
+	C.EnableLedControl(C.bool(bodyParsed.Enabled), C.DEVICE_INDEX(devInt))
+
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte("{}"))
+	if err != nil {
+		errorLogger.Fatal("Cannot write response!")
+	}
+}
+
+func putDeviceColor(w http.ResponseWriter, _ *http.Request, params httprouter.Params) {
+	var dev = params.ByName("device")
+	devInt, err := strconv.Atoi(dev)
+	if err != nil {
+		errorLogger.Printf("Device %s unknown!", dev)
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := w.Write([]byte("{}"))
+		if err != nil {
+			errorLogger.Fatal("Cannot write response!", err)
+		}
+		return
+	}
+
+	C.SetFullLedColor(255, 255, 255, C.DEVICE_INDEX(devInt))
+
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte("{}"))
+	if err != nil {
+		errorLogger.Fatal("Cannot write response!")
+	}
+}
+
+func initMux(router *httprouter.Router, prefix string) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle(prefix+"/", http.StripPrefix(prefix, router))
+	return mux
+}
 
 func main() {
-	fmt.Println("hello from go!")
+	defer C.EnableLedControl(false, 1)
+
+	router := httprouter.New()
+	router.GET("/hello", rootHandler)
+	router.PUT("/devices/:device", putDeviceLedControl)
+	router.PUT("/devices/:device/color", putDeviceColor)
+	mux := initMux(router, "/api/v1")
+
+	err := http.ListenAndServe(":10007", mux)
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
 }
